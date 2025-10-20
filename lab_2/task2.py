143a4f76c53326bc50821106ea9f144a9429cfc1
Elizabeth <pirogelizaveta237@gmail.com>
2025-10-20 23:31:21 +0300

    регрессия: разделение на обучающую и тестовую выборки непрерывного признака

--- lab_2/task2.py
+++ lab_2/task2.py
@@ -22,3 +22,13 @@ X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.4, random_
 
 print(f"Обучающая выборка для Transported: {X_train.shape[0]} samples ({X_train.shape[0]/len(X)*100:.1f}%)")
 print(f"Тестовая выборка для Transported: {X_test.shape[0]} samples ({X_test.shape[0]/len(X)*100:.1f}%)")
+
+#регрессия для age
+
+X_a = df.drop(['Age'], axis=1)
+y_a = df['Age']
+
+X_a_train, X_a_test, y_a_train, y_a_test = train_test_split(X_a, y_a, test_size=0.4, random_state=42)
+
+print(f"Обучающая выборка для Age: {X_a_train.shape[0]} samples ({X_a_train.shape[0]/len(X)*100:.1f}%)")
+print(f"Тестовая выборка для Age: {X_a_test.shape[0]} samples ({X_a_test.shape[0]/len(X)*100:.1f}%)")
