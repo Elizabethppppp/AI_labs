c1eb533fccf6b5bada51cf126889944426e58c7c
Elizabeth <pirogelizaveta237@gmail.com>
2025-10-20 23:00:51 +0300

    import libraries

---
+++ lab_2/task2.py
@@ -0,0 +1,14 @@
+import pandas as pd
+
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression
+from sklearn.metrics import mean_squared_error
+from sklearn.metrics import root_mean_squared_error
+from sklearn.metrics import mean_absolute_error
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import accuracy_score
+from sklearn.metrics import confusion_matrix
+from sklearn.metrics import classification_report
