1f2b501c033d7ba319b141307dfba5493d64a607
Elizabeth <pirogelizaveta237@gmail.com>
2025-10-27 21:53:20 +0300

    добавлена часть кода из 2 лабы, где были действия с регрессионной моделью

---
+++ lab_3/task3.py
@@ -0,0 +1,44 @@
+import pandas as pd
+
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression
+from sklearn.metrics import mean_squared_error
+from sklearn.metrics import root_mean_squared_error
+from sklearn.metrics import mean_absolute_error
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import accuracy_score
+from sklearn.metrics import confusion_matrix
+from sklearn.metrics import classification_report
+
+
+df = pd.read_csv("processed_titanic.csv")
+
+df_k = pd.get_dummies(df, drop_first=True)
+df_clean = df_k.dropna()
+
+#регрессия для age
+
+X_a = df_clean.drop(['Age'], axis=1)
+y_a = df_clean['Age']
+
+X_a_train, X_a_test, y_a_train, y_a_test = train_test_split(X_a, y_a, test_size=0.4, random_state=42)
+
+print(f"Обучающая выборка для Age: {X_a_train.shape[0]} samples ({X_a_train.shape[0]/len(X)*100:.1f}%)")
+print(f"Тестовая выборка для Age: {X_a_test.shape[0]} samples ({X_a_test.shape[0]/len(X)*100:.1f}%)")
+
+linear_model = LinearRegression()
+linear_model.fit(X_a_train, y_a_train)
+y_pred_test = linear_model.predict(X_a_test)
+
+#оценка работы регрессионной модели
+
+MSE = mean_squared_error(y_a_test, y_pred_test)
+RMSE = root_mean_squared_error(y_a_test, y_pred_test)
+MAE = mean_absolute_error(y_a_test, y_pred_test)
+
+print(f"Среднеквадратичная ошибка: {MSE:.2f}")
+print(f"Корень среднеквадратичной ошибки: {RMSE:.2f}")
+print(f"Средняя абсолютная ошибка: {MAE:.2f}")
