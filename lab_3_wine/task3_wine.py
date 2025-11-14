1b235a93dfa715e2c93a8470fa3e92a744ae9f5a
Elizabeth <pirogelizaveta237@gmail.com>
2025-11-14 09:46:57 +0300

    оценка регрессии

--- lab_3_wine/task3_wine.py
+++ lab_3_wine/task3_wine.py
@@ -25,3 +25,12 @@ X_reg_train, X_reg_test, y_reg_train, y_reg_test = train_test_split(X_reg, y_reg
 reg_tree = DecisionTreeRegressor(max_depth=4, random_state=42)
 reg_tree.fit(X_reg_train, y_reg_train)
 y_reg_pred = reg_tree.predict(X_reg_test)
+
+#оценка работы регрессионной модели
+MSE = mean_squared_error(y_reg_test, y_reg_pred)
+RMSE = root_mean_squared_error(y_reg_test, y_reg_pred)
+MAE = mean_absolute_error(y_reg_test, y_reg_pred)
+
+print(f"Среднеквадратичная ошибка: {MSE:.2f}")
+print(f"Корень среднеквадратичной ошибки: {RMSE:.2f}")
+print(f"Средняя абсолютная ошибка: {MAE:.2f}")
