6d779767805aa6392cce940b23c0bdaa961e32dc
Elizabeth <pirogelizaveta237@gmail.com>
2025-11-14 09:43:20 +0300

    выбран параметр и разделен датесет на выборки

--- lab_3_wine/task3_wine.py
+++ lab_3_wine/task3_wine.py
@@ -10,3 +10,14 @@ from sklearn.metrics import auc
 from sklearn.metrics import roc_curve
 
 df = pd.read_csv("WineQT.csv")
+
+df = df.drop("Id", axis=1)
+
+#регрессия для алкоголя
+
+target_reg = "alcohol"
+X_reg = df.drop(columns=[target_reg])
+y_reg = df[target_reg]
+
+X_reg_train, X_reg_test, y_reg_train, y_reg_test = train_test_split(X_reg, y_reg, test_size=0.3, random_state=42)
+
