aa1cc47f3347d6977eed8eb2484f110b9a1d737c
Elizabeth <pirogelizaveta237@gmail.com>
2025-11-14 09:28:08 +0300

    библиотеки

---
+++ lab_3_wine/task3_wine.py
@@ -0,0 +1,7 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import mean_squared_error, mean_absolute_error, roc_curve, auc
+from sklearn.tree import DecisionTreeRegressor, DecisionTreeClassifier, plot_tree
+from sklearn.preprocessing import StandardScaler
+
