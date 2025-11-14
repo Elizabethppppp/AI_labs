177f8b2859a0832b90dd5a634d8e36046a71cf72
Elizabeth <pirogelizaveta237@gmail.com>
2025-11-14 09:39:14 +0300

    добавлены ещё библиотеки

--- lab_3_wine/task3_wine.py
+++ lab_3_wine/task3_wine.py
@@ -1,8 +1,12 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split
-from sklearn.metrics import mean_squared_error, mean_absolute_error, roc_curve, auc
-from sklearn.tree import DecisionTreeRegressor, DecisionTreeClassifier, plot_tree
-from sklearn.preprocessing import StandardScaler
+from sklearn.metrics import mean_squared_error
+from sklearn.metrics import root_mean_squared_error
+from sklearn.metrics import mean_absolute_error
+from sklearn.tree import DecisionTreeRegressor, plot_tree
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.metrics import auc
+from sklearn.metrics import roc_curve
 
 df = pd.read_csv("WineQT.csv")
