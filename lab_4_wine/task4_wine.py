a70941eb7a1f716f7f0467414a9204956f71028a
Elizabeth <pirogelizaveta237@gmail.com>
2025-11-15 19:00:26 +0300

    объект

--- lab_4_wine/task4_wine.py
+++ lab_4_wine/task4_wine.py
@@ -1,4 +1,5 @@
 import pandas as pd
+from sklearn.ensemble import RandomForestClassifier
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
 
@@ -17,3 +18,7 @@ X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_
 scaler = StandardScaler()
 X_train_scaled = scaler.fit_transform(X_train)
 X_test_scaled = scaler.transform(X_test)
+
+#задачa классификации методом случайного леса
+rf = RandomForestClassifier(n_estimators=300, oob_score=True, random_state=42, n_jobs=-1)
+
