2868d54764488f7c9010907ac02658283ffd3677
Elizabeth <pirogelizaveta237@gmail.com>
2025-11-15 18:51:53 +0300

    разделение на выборки

--- lab_4_wine/task4_wine.py
+++ lab_4_wine/task4_wine.py
@@ -1,4 +1,5 @@
 import pandas as pd
+from sklearn.model_selection import train_test_split
 
 df = pd.read_csv("WineQT.csv")
 print("Информация о датасете:")
@@ -9,3 +10,5 @@ df = df.drop(columns=['Id'])
 df['label'] = (df['quality'] >= 6).astype(int)
 X = df.drop(columns=['quality', 'label'])
 y = df['label']
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42, stratify=y)
